1858cb85f03736d2eeb477f41a942851bae91144
axelboc <axelboc@gmail.com>
2020-09-22 09:10:00 +0000

    Deploying to gh-pages from  @ f19617e282f1d2a119f16c7804f53a68a47435a0 🚀

---
+++ precache-manifest.aa0ca7c6c4e729c58a8552b035807844.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d91bd3261aaba46390c04b2a321b3822",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.b205c6b41495dc6c8547.bundle.js"
+  },
+  {
+    "url": "runtime~main.b205c6b41495dc6c8547.bundle.js"
+  },
+  {
+    "revision": "19877932fe395ca2d277",
+    "url": "static/css/main.d6232dc8.chunk.css"
+  },
+  {
+    "url": "vendors~main.b205c6b41495dc6c8547.bundle.js"
+  },
+  {
+    "url": "vendors~main.b205c6b41495dc6c8547.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "d133365006a25d8b2502936884722974",
+    "url": "worker.70da18550c0dc5f17833.bundle.worker.js"
+  }
+]);
