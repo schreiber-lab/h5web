686900848f30e6fccf6c35fcdf56b75831fd7971
axelboc <axelboc@gmail.com>
2020-09-25 14:32:41 +0000

    Deploying to gh-pages from  @ a2a53e781a4bfd16aa54f96ecd7ec8bb92dac98a 🚀

---
+++ precache-manifest.6d1f8ca7e118f7a408b20c9664485ab5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "916d7efd0c547f13d7eaf99154d40393",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.8001a66c1705226cf21c.bundle.js"
+  },
+  {
+    "url": "runtime~main.8001a66c1705226cf21c.bundle.js"
+  },
+  {
+    "revision": "41fa1d830691a53a1d4c",
+    "url": "static/css/main.2337de56.chunk.css"
+  },
+  {
+    "url": "vendors~main.8001a66c1705226cf21c.bundle.js"
+  },
+  {
+    "url": "vendors~main.8001a66c1705226cf21c.bundle.js.LICENSE.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.02ba0598a28662401b99bcdb7c95f019.js"
+  "precache-manifest.6d1f8ca7e118f7a408b20c9664485ab5.js"
 );
 
 self.addEventListener('message', (event) => {
