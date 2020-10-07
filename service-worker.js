c1ec09e2b663b04c554dcef3d1e3095c51fd6b5d
axelboc <axelboc@gmail.com>
2020-10-07 13:59:37 +0000

    Deploying to gh-pages from  @ d1013d730d466e3b0f6c9d1ad1218c11c0e08d76 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.7ca71b2d9830acc4c0d08c8a02666ba2.js"
+  "precache-manifest.7fd7ba992af0a2e53b4f340e0a01544b.js"
 );
 
 self.addEventListener('message', (event) => {
