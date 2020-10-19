69b05d450e9dd412a21860a875e22b15df31bcfb
loichuder <42204205+loichuder@users.noreply.github.com>
2020-10-19 13:06:02 +0000

    Deploying to gh-pages from  @ 615d964048fa5b4ce57bbd6135fbad71c62d63a3 🚀

---
+++ precache-manifest.3c5fa72131c6730a9bdda3027ab138fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1fb707c56525da2e7ae2796ca583b22",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.1f00dfbbe5464c176fd2.bundle.js"
+  },
+  {
+    "url": "runtime~main.1f00dfbbe5464c176fd2.bundle.js"
+  },
+  {
+    "revision": "33b44208bfec586c2b8f",
+    "url": "static/css/main.406b95c7.chunk.css"
+  },
+  {
+    "url": "vendors~main.1f00dfbbe5464c176fd2.bundle.js"
+  },
+  {
+    "url": "vendors~main.1f00dfbbe5464c176fd2.bundle.js.LICENSE.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.9d07738e870c8ccd648d6e2342d714bb.js"
+  "precache-manifest.3c5fa72131c6730a9bdda3027ab138fa.js"
 );
 
 self.addEventListener('message', (event) => {
